000a87dbc3b424306793b3b28d5cb77d29ba7a57
Pål Grønås Drange <PGDR@statoil.com>
2017-09-20 22:58:38 +0200

    Transaction knows about Message

--- python/stockchain/transaction.py
+++ python/stockchain/transaction.py
@@ -1,3 +1,5 @@
+from .message import Message
+
 class Transaction(object):
     def __init__(self, message, signed, double_signed, from_user, to_user):
         self.message = message
@@ -19,6 +21,6 @@ class Transaction(object):
     @staticmethod
     def parse(trans):
         action, stock, amount, price, created, expires = trans.split('!')[:6]
-        message = '!'.join([action, stock, amount, price, created, expires])
+        message = Message('!'.join([action, stock, amount, price, created, expires]))
         signed, double_signed, from_user, to_user = trans.split('!')[6:]
         return Transaction(message, signed, double_signed, from_user, to_user)
